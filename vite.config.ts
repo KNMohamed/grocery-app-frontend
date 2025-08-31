1de6187b856e66a0859eafefa75746b7c330e134
Khalid Mohamed <khalid.n.mohamed@outlook.com>
2025-08-31 00:52:57 -0400

    Install react-testing-library prettier vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  test: {
+    // ...
+  },
   plugins: [
     react(),
     tailwindcss(),
